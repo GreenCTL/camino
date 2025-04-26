18081369f3c561de37d6f63fb2afac9ec350777b
GreenCTL <aaaaaa55589@gmail.com>
2025-04-27 02:28:42 +0800

    remove 3001

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
